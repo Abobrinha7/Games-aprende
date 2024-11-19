6fc1c7a56a2bf91175126e1fbeac1222668b668d
Ana Beatriz <00001080238992sp@al.educacao.sp.gov.br>
2024-11-19 17:40:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'jogos',
+    'O que significa FPS nos jogos?',
+    'Frames por segundo'
+)
+
+criaCartao(
+    'jogos',
+    'O que significa NPC?',
+    'Non-Playable Character'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual empresa criou o PlayStation?',
+    'Sony'
+)
+
+criaCartao(
+    'jogos',
+    'Qual o nome do herói de The Legend of Zelda?',
+    'Link'
+)
